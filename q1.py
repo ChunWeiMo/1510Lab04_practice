a77f2d091c0b8c90d0e14240ab93feb3a578ef45
lionheartmo2 <lionheartmo2@gmail.com>
2023-10-04 16:59:11 -0700

    Complete question 1

--- q1.py
+++ q1.py
@@ -1,1 +1,31 @@
-print("hello")
+def prime(number):
+    """
+    Calculate factorial.
+
+    A simple function that check if a positive integer is prime.
+
+    :param number: a positive integer
+    :precondition: number must be a positive integer greater than 1
+    :postcondition: check number
+    :return: a boolean 
+    """
+
+    divisor = 2
+    if number == 1:
+        is_prime = False
+    elif number == 2:
+        is_prime = True
+    else:
+        while divisor < number:
+            if number % divisor == 0:
+                is_prime = False
+                break
+            else:
+                is_prime = True
+            divisor += 1
+
+    return is_prime
+
+
+if __name__ == "__main__":
+    print(prime(53))
