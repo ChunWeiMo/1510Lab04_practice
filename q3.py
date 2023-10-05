b07ee3ce91ee656b906389c4239e90d5dfac82d8
Chun-Wei, Mo <chunwei.mo@gmail.com>
2023-10-05 13:02:11 -0700

    Add 2 functions: shift_capital_letter and shift_lower_letter

--- q3.py
+++ q3.py
@@ -1,3 +1,27 @@
+def shift_capital_letter(letter, shift):
+    # print("letter= ", letter)
+    # print("shift= ", shift)
+    letter += shift
+    if letter > 90:
+        letter = letter - 26
+    elif letter < 65:
+        letter = letter + 26
+    # print("letter after shift", letter)
+    return letter
+
+
+def shift_lower_letter(letter, shift):
+    # print("letter= ", letter)
+    # print("shift= ", shift)
+    letter += shift
+    if letter > 122:
+        letter = letter - 26
+    elif letter < 97:
+        letter = letter + 26
+    # print("letter after shift", letter)
+    return letter
+
+
 def caesarcipher(message, encode, shift):
     """
     Convert a base 10 number to destination bass.
@@ -17,33 +41,27 @@ def caesarcipher(message, encode, shift):
 
     for character in message:
         print(character)
-        encoded_character = ord(character)
+        character = ord(character)
+        print(character)
         # ASCII code A ~ Z : 65 ~ 90
-        if encoded_character >= 65 and encoded_character <= 90:
+        # # ASCII code a ~ z : 97 ~ 122
+        if character >= 65 and character <= 90:
             print("into capital case")
-            encoded_character += shift
-            if encoded_character > 90:
-                encoded_character = encoded_character - 26
-            elif encoded_character <65:
-                encoded_character = encoded_character + 26
-        
-        #            a ~ z : 97 ~ 122
-        elif encoded_character >= 97 and encoded_character <= 122:
+            encoded_character = shift_capital_letter(character, shift)
+        elif character >= 97 and character <= 122:
             print("into lower case")
-            encoded_character += shift
-            if encoded_character > 122:
-                encoded_character = encoded_character - 26
-            elif encoded_character < 97:
-                encoded_character = encoded_character + 26
+            encoded_character = shift_lower_letter(character, shift)
+        else:
+            encoded_character = character
 
         message_after_cipher += chr(encoded_character)
-            
+
     return message_after_cipher
 
 
 def main():
-    print(caesarcipher("cAt", False, 2))
+    print(caesarcipher("catCAT", True, 2))
+
 
 if __name__ == "__main__":
     main()
-    # print(ord("a"))
