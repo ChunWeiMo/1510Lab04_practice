df2ae30608f83ce8ef2533c675194d86783b7139
lionheartmo2 <lionheartmo2@gmail.com>
2023-10-08 01:26:48 -0700

    Add docstring and doctest

--- q2.py
+++ q2.py
@@ -1,13 +1,20 @@
 def base_convert(base_10_number, destination_base):
     """
-    Convert a base 10 number to destination bass.
+    Convert a base 10 number to destination base.
 
-    :param base_10_number: an integer >= 0
-    :param destination_base: a positive integer [2,10]
-    :precondition: 
-    :postcondition: 
-    :return: a integer
+    :param base_10_number: an integer 
+    :param destination_base: an integer
+    :precondition: base_10_number must be a decimal and positive integer
+    :precondition: destination_base must be inclusive and within range of 2 to 10
+    :postcondition: convert base_10_number to a number with destination_base
+    :return: a number with destination_base
+
+    >>> base_convert(13, 2)
+    1101
+    >>> base_convert(1988, 5)
+    30421
     """
+
     converted_number = ""
     while (base_10_number != 0):
         digit_to_concatenate = base_10_number % destination_base
@@ -18,8 +25,10 @@ def base_convert(base_10_number, destination_base):
 
 
 def main():
-    print(base_convert(9487, 7))
-
+    print("9487 convert to base 7: ",base_convert(9487, 7))
+    print("54088 convert to base 3: ",base_convert(54088, 3))
+    print("617 convert to base 9: ",base_convert(617, 9))
+    
 
 if __name__ == "__main__":
     main()
