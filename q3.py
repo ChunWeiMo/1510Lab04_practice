2cd91f7fee6cfbabacf03078be6cf7a2d64171a0
lionheartmo2 <lionheartmo2@gmail.com>
2023-10-08 00:38:43 -0700

    Add docstring and doctest

--- q3.py
+++ q3.py
@@ -1,38 +1,76 @@
-def shift_capital_letter_unicode(letter, shift):
+def shift_uppercase_letter(uppercase_letter, shift):
     """
-    >>> shift_capital_letter(78, 3)
-    81
-    >>> shift_capital_letter(89, 10)
-    73
+    Return a uppercase letter after a unicode shift.
+
+    :param uppercase_letter: a string 
+    :param shift: an integer
+    :precondition: uppercase_letter must be a single character and a uppercase letter
+    :precondition: shift can be a positive or negative integer
+    :postcondition: find the uppercase letter after the Unicode shift
+    :return: a shifted uppercase letter
+
+    >>> shift_uppercase_letter("Z", 3)
+    "C"
+    >>> shift_uppercase_letter("D", -2)
+    "B"
     """
-    letter += shift
-    if letter > 90:
-        letter = letter - 26
-    elif letter < 65:
-        letter = letter + 26
-    return letter
+    uppercase_letter = ord(uppercase_letter)
+    uppercase_letter += shift
+    if uppercase_letter > 90:
+        uppercase_letter -= - 26
+    elif uppercase_letter < 65:
+        uppercase_letter += 26
+    return chr(uppercase_letter)
 
 
-def shift_lower_letter_unicode(letter, shift):
-    letter += shift
-    if letter > 122:
-        letter = letter - 26
-    elif letter < 97:
-        letter = letter + 26
-    return letter
+def shift_lowercase_letter(lowercase_letter, shift):
+    """
+    Return a lowercase letter after a unicode shift.
+
+    :param lowercase_letter: a string
+    :param shift: an integer
+    :precondition: lowercase_letter must be a single character and a lowercase letter
+    :precondition: shift can be a positive or negative integer
+    :postcondition: find the lowercase letter after the Unicode shift
+    :return: a shifted lowercase letter
+
+    >>> shift_lowercase_letter("c", 3)
+    "f"
+    >>> shift_lowercase_letter("a", -2)
+    "y"
+    """
+    lowercase_letter = ord(lowercase_letter)
+    lowercase_letter += shift
+    if lowercase_letter > 122:
+        lowercase_letter -= 26
+    elif lowercase_letter < 97:
+        lowercase_letter += 26
+    return chr(lowercase_letter)
 
 
 def caesarcipher(message, encode, shift):
     """
-    Convert a base 10 number to destination bass.
+    Encode or discover a message.
 
-    param message:
-    param encode:
-    param shift: 
-    precondition: 
-    postcondition: 
-    return: a integer
+    A function encodes or discovers a message through a Unicode shift.
+
+    :param message: a string
+    :param encode: a boolean
+    :param shift: an integer
+    :precondition: message may be empty
+    :precondition: encode must be True or False
+    :precondition: shift can be a positive or negative integer
+    :postcondition: encode or discover message through the Unicode shift
+    :return: encoded or discovered message
+
+    >>>caesarcipher("Chris", True, 2)
+    "Ejtku"
+    >>>caesarcipher("cat", True, 7)
+    "jha"
+    >>>caesarcipher("cat", True, -2)
+    "ayr"
     """
+
     message_after_cipher = ""
 
     # decode
@@ -40,27 +78,25 @@ def caesarcipher(message, encode, shift):
         shift *= -1
 
     for character in message:
-        character = ord(character)
-        # unicode A ~ Z : 65 ~ 90
-        # # unicode a ~ z : 97 ~ 122
-        if character >= 65 and character <= 90:
-            encoded_character = shift_capital_letter_unicode(character, shift)
-        elif character >= 97 and character <= 122:
-            encoded_character = shift_lower_letter_unicode(character, shift)
+        # Unicode A ~ Z : 65 ~ 90
+        # Unicode a ~ z : 97 ~ 122
+        if ord(character) >= 65 and ord(character) <= 90:
+            encoded_character = shift_uppercase_letter(character, shift)
+        elif ord(character) >= 97 and ord(character) <= 122:
+            encoded_character = shift_lowercase_letter(character, shift)
         else:
             encoded_character = character
 
-        message_after_cipher += chr(encoded_character)
+        message_after_cipher += encoded_character
 
     return message_after_cipher
 
 
 def main():
-    print(caesarcipher("catCAT", True, 2))
+    print(caesarcipher("cat", True, 7))
+    print(caesarcipher("CAT", True, -2))
+    print(caesarcipher("F-Z, Pr", False, 3))
 
 
 if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
-
     main()
