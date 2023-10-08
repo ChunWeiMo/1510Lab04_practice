8bae83901cb97f582780a2e948944da7d7551b78
lionheartmo2 <lionheartmo2@gmail.com>
2023-10-07 23:07:30 -0700

    Delete debugger print() and Change 2 shift function name

--- q3.py
+++ q3.py
@@ -1,24 +1,24 @@
-def shift_capital_letter(letter, shift):
-    # print("letter= ", letter)
-    # print("shift= ", shift)
+def shift_capital_letter_unicode(letter, shift):
+    """
+    >>> shift_capital_letter(78, 3)
+    81
+    >>> shift_capital_letter(89, 10)
+    73
+    """
     letter += shift
     if letter > 90:
         letter = letter - 26
     elif letter < 65:
         letter = letter + 26
-    # print("letter after shift", letter)
     return letter
 
 
-def shift_lower_letter(letter, shift):
-    # print("letter= ", letter)
-    # print("shift= ", shift)
+def shift_lower_letter_unicode(letter, shift):
     letter += shift
     if letter > 122:
         letter = letter - 26
     elif letter < 97:
         letter = letter + 26
-    # print("letter after shift", letter)
     return letter
 
 
@@ -26,12 +26,12 @@ def caesarcipher(message, encode, shift):
     """
     Convert a base 10 number to destination bass.
 
-    :param message:
-    :param encode:
-    :param shift: 
-    :precondition: 
-    :postcondition: 
-    :return: a integer
+    param message:
+    param encode:
+    param shift: 
+    precondition: 
+    postcondition: 
+    return: a integer
     """
     message_after_cipher = ""
 
@@ -40,17 +40,13 @@ def caesarcipher(message, encode, shift):
         shift *= -1
 
     for character in message:
-        print(character)
         character = ord(character)
-        print(character)
-        # ASCII code A ~ Z : 65 ~ 90
-        # # ASCII code a ~ z : 97 ~ 122
+        # unicode A ~ Z : 65 ~ 90
+        # # unicode a ~ z : 97 ~ 122
         if character >= 65 and character <= 90:
-            print("into capital case")
-            encoded_character = shift_capital_letter(character, shift)
+            encoded_character = shift_capital_letter_unicode(character, shift)
         elif character >= 97 and character <= 122:
-            print("into lower case")
-            encoded_character = shift_lower_letter(character, shift)
+            encoded_character = shift_lower_letter_unicode(character, shift)
         else:
             encoded_character = character
 
@@ -64,4 +60,7 @@ def main():
 
 
 if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+
     main()
